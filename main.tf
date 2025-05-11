4ae270b53288b81ba99258d842de4230c803150d
Zain <154006182+0xZainRaza@users.noreply.github.com>
2025-05-11 02:37:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
@@ -18,31 +17,26 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-
 #Creating a route table
 
 resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"  #This will send all the ipv4 traffic where even it routes 
+    cidr_block = "0.0.0.0/0"  
     gateway_id = aws_internet_gateway.gw.id
   }
-
-  
 
   tags = {
     Name = "example"
   }
 }
 
-
 # Setting up the Subnet
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # This basically sets your data in two different region if one data center
-                                   # is down you are still good to go 
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Subnet"
@@ -55,24 +49,22 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.example.id
 }
 
-
-#Creating Security Group baciscally a ACL
-
+#Creating Security Group with necessary ports
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
-  description = "Allow TLS Web traffic from port 443, HTTP traffic from port 80, and SSH traffic from port 22 from anywhere"
+  description = "Allow traffic for HTTP, HTTPS, SSH, Grafana, Prometheus, and Loki"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -84,29 +76,43 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3100
+    to_port     = 3100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-
 # Creating a network interface
-
-
 resource "aws_network_interface" "test" {
   subnet_id       = aws_subnet.main.id
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_tls.id]
-
-  
 }
 
-
-# Assiging a elastic ip
-
+# Assigning an elastic IP
 resource "aws_eip" "one" {
   domain                    = "vpc"
   network_interface         = aws_network_interface.test.id
@@ -114,10 +120,9 @@ resource "aws_eip" "one" {
   depends_on = [ aws_instance.foo , aws_internet_gateway.gw ]
 }
 
-# Deploy a EC2 Ubuntu
-
+# Deploying EC2 Ubuntu
 resource "aws_instance" "foo" {
-  ami           = "ami-04b70fa74e45c3917" # us-west-2
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
 
   availability_zone = "us-east-1a"
@@ -126,34 +131,24 @@ resource "aws_instance" "foo" {
   network_interface {
     device_index = 0
     network_interface_id = aws_network_interface.test.id
-
   }
 
   user_data = <<-EOF
-                #!/bin/bash
-    
-                sudo apt-get update
-                sudo apt-get install -y git docker.io docker-compose
-    
-                # Clone the Git repository
-                git clone https://github.com/0xZainRaza/Secure-File-Scan /home/ubuntu/Secure-File-Scan
-    
-                # Change directory into the cloned repository
-                cd /home/ubuntu/Secure-File-Scan
-    
-                # Build the Docker image
-                sudo docker build -t my-secure-file-scan .
-    
-                # Run the Docker container
-                sudo docker-compose up -d 
-                 EOF
-      tags = {
-        Name = "Web-server"
-      }
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y git docker.io docker-compose
+    git clone https://github.com/0xZainRaza/Secure-File-Scan /home/ubuntu/Secure-File-Scan
+    cd /home/ubuntu/Secure-File-Scan
+    sudo docker build -t my-secure-file-scan .
+    sudo docker-compose up -d
+  EOF
+
+  tags = {
+    Name = "Web-server"
+  }
 }
 
-
- # Output the server's public IP address
+# Output the server's public IP address
 output "server_public_ip" {
   value = aws_instance.foo.public_ip
 }
